7eaf70c2fbb5c565377968ff8ec658d0dff3fe3f
whisperrrr <jh731448520@163.com>
2020-10-27 23:28:33 +0800

    [Jin Hui][feat]- add show function to group

--- src/App/App.js
+++ src/App/App.js
@@ -7,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        <p>hello world</p>
         <StudentTeamCard />
         <StudentListCard />
       </div>
--- src/App/components/StudentTeamCard.js
+++ src/App/components/StudentTeamCard.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './StudentTeamCard.css';
 
 function StudentTeamCard() {
+  const [isDivide, setIeDivide] = useState(false);
+
+  function divideToGroup() {
+    setIeDivide(true);
+  }
+
   return (
     <div className="student_team">
       <div className="student_team_title">
         <span>分组列表</span>
-        <button type="button">分组学员</button>
+        <button type="button" onClick={divideToGroup}>
+          分组学员
+        </button>
       </div>
+      {isDivide && <div>分组了哟</div>}
     </div>
   );
 }
