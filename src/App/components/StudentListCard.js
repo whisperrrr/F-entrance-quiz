bd38d9b5726cbfffa279cab88b56458be9b6f1ce
whisperrrr <jh731448520@163.com>
2020-10-27 22:40:52 +0800

    [Jin Hui][feat]- map the backend data to map component

--- src/App/components/StudentListCard.js
+++ src/App/components/StudentListCard.js
@@ -1,24 +1,25 @@
-import React from 'react';
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function StudentListCard() {
-  // const [data, setData] = useState();
-  //
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios(
-  //       'http://localhost:8080/students'
-  //     );
-  //     setData(result.data);
-  //     console.log(result.data);
-  //   };
-  //
-  //   fetchData();
-  //
-  // }, []);
+  const [data, setData] = useState({ students: [{ id: '', name: '' }] });
 
-  return <div>我是小组分组卡</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('http://localhost:8080/students');
+      setData(result.data);
+      console.log(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {data.students.map((i) => (
+        <li key={i.id}>{i.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default StudentListCard;
