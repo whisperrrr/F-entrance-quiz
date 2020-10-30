12462500ec9f03b3cf76a0565cddbb86698a46de
whisperrrr <jh731448520@163.com>
2020-10-30 14:03:49 +0800

    [Jin Hui][feat]- add onchange function on input

--- src/App/components/StudentListCard.js
+++ src/App/components/StudentListCard.js
@@ -5,20 +5,33 @@ import './StudentListCard.css';
 function StudentListCard() {
   const [data, setData] = useState({ students: [{ id: '', name: '' }] });
 
+  const fetchData = async () => {
+    const result = await axios('http://localhost:8080/students');
+    setData(result.data);
+  };
+
+  const postStudent = async (student) => {
+    await axios.post('http://localhost:8080/newstudent', student).then(fetchData());
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios('http://localhost:8080/students');
-      setData(result.data);
-    };
     fetchData();
   }, []);
+
+  const handleEnterDown = (event) => {
+    if (event.keyCode === 13) {
+      console.log(event.target.value);
+      postStudent(event.target.value);
+      fetchData();
+    }
+  };
 
   return (
     <div className="student-list-card">
       {data.students.map((i) => (
         <div className="student_card" key={i.id}>{`${i.id}.${i.name}`}</div>
       ))}
-      <input type="text" name="new-student" />
+      <input type="text" name="newStudent" onKeyDown={handleEnterDown} />
     </div>
   );
 }
